d6f8d9a93399b20c5e07eebc7c3e161cd2e13934
Seif <sei4or@gmail.com>
2020-04-14 12:31:23 -0700

    Fixed starred message count check

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function saveRecording(voiceReceiver) {
 
 client.on("ready", () => {
     console.log("Ready. I guess...");
-    client.editStatus({name: "idk", type: 3});
+    client.editStatus({name: "the people here", type: 3});
 });
 
 // COMMANDS
@@ -193,19 +193,28 @@ client.on("messageReactionAdd", async (message, emoji, userID) => {
     let guild = client.guilds.get(client.channelGuildMap[message.channel.id]);
     let channel = guild.channels.get(message.channel.id);
     
-    let latestMessage = await channel.getMessage(message.id);
-    if (latestMessage.reactions.size > 1) return;
+    let starredMessage;
+    if (message.reactions != null) {
+        if (message.reactions[starEmoji] == null) return;
+        if (message.reactions[starEmoji].count > 1) return;
+        starredMessage = message;
+    } else {
+        let retrievedMessage = await channel.getMessage(message.id);
+        if (retrievedMessage.reactions[starEmoji] == null) return;
+        if (retrievedMessage.reactions[starEmoji].count > 1) return;
+        starredMessage = retrievedMessage;
+    }
 
     let embed = new EmbedBuilder()
-    .setAuthor(`${latestMessage.author.username}#${latestMessage.author.discriminator}`, null, latestMessage.author.avatarURL)
-    .setDescription(latestMessage.content.length > 0 ? latestMessage.content : "Unknown")
-    .addField("Quick Link", `[Click Here](${linkMessage(latestMessage)})`)
-    .setFooter(latestMessage.id)
+    .setAuthor(`${starredMessage.author.username}#${starredMessage.author.discriminator}`, null, starredMessage.author.avatarURL)
+    .setDescription(starredMessage.content.length > 0 ? starredMessage.content : "Unknown")
+    .addField("Quick Link", `[Click Here](${linkMessage(starredMessage)})`)
+    .setFooter(starredMessage.id)
     .setTimestamp(new Date());
 
-    if (latestMessage.attachments.length > 0) embed.setImage(latestMessage.attachments[0].url);
-    if (latestMessage.embeds.length > 0) {
-        if (latestMessage.embeds[0].description != null) embed.setDescription(`> ${latestMessage.embeds[0].description}`);
+    if (starredMessage.attachments.length > 0) embed.setImage(starredMessage.attachments[0].url);
+    if (starredMessage.embeds.length > 0) {
+        if (starredMessage.embeds[0].description != null) embed.setDescription(`> ${starredMessage.embeds[0].description}`);
     }
 
     guild.channels.get(starboard).createMessage({content: `<#${channel.id}>`, embed: embed.render()});
