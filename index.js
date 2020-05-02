35408c079b145e9ca99ab8996c75d9c4f2249686
Sei <Sei4or@gmail.com>
2020-05-02 10:39:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ client.on("messageCreate", message => {
         message.channel.createMessage("Reading from this channel.");
     }
     if (cmd === "rate") {
-        message.channel.createMessage("**sithsiri#3253** has sent the most messages on the server. Last check resulted in 70,925 messages.");
+        message.channel.createMessage("**sithsiri#3253** has sent the most messages on the server. Last check resulted in 87,778 messages.");
     }
     if (cmd === "ttsvolume") {
         let number = parseInt(args[1]);
