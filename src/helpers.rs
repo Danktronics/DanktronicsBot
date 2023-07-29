a71fa8aad2118a62053b8539a282fa76e8eb46ef
Sithindu Sirisooriya <sith@live.com>
2023-07-29 18:11:34 -0500

    Fix my mistakes :(((

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::HashMap;
 use serenity::model::id::ChannelId;
 use serenity::model::channel::{Message, Channel};
 use serenity::cache::Cache;
